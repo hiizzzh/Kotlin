b515c206f89558900fe98577638ab7dd69b399e2
iliahkhizhn <world170805@gmail.com>
2023-10-19 23:27:14 +0300

    easy examle of using an abstaction class

---
+++ src/main/kotlin/abstraction.kt
@@ -0,0 +1,22 @@
+abstract class SomeAbstractClass {
+    abstract fun abstractMethod()
+    fun callAbstractTwice() {
+        abstractMethod() // You can use abstract methods inside the class, because it is assumed they
+        // will be overridden in the child class.
+        abstractMethod()
+    }
+}
+
+class SomeRegularClass : SomeAbstractClass() {
+    override fun abstractMethod() {
+        println("Calling abstract method")
+    }
+}
+
+fun main() {
+    val regular = SomeRegularClass()
+    regular.abstractMethod() // Calling abstract method
+    regular.callAbstractTwice()
+    // Calling abstract method
+    // Calling abstract method
+}
